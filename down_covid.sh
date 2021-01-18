e5af1000c172a498e1a275dbff4263a64e6db68f
idaios <pavlidisp@gmail.com>
2021-01-18 17:20:34 +0200

    script for covid data

---
+++ down_covid.sh
@@ -0,0 +1,9 @@
+#how to download fastg file
+#go to firefox and search ncbi covid-19
+#chose a file and go to bash
+#run the command below
+
+fastq-dump --split-files SRR13448536
+
+#now i have two files because my data are two-paired
+ 
